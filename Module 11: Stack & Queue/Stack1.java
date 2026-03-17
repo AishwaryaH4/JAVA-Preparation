e27531b85f44bbc00f5cc0bb2878a10368d3b219
Aishwarya Hegde <aishmhegde@gmail.com>
2026-03-17 07:36:48 +0530

    Add files via upload

---
+++ Module 11: Stack & Queue/Stack1.java
@@ -0,0 +1,31 @@
+// LeetCode 20 – Valid Parentheses
+
+import java.util.*;
+
+class Solution {
+    public boolean isValid(String s) {
+
+        Stack<Character> st = new Stack<>();
+
+        for(char c : s.toCharArray()){
+
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+
+                if(st.isEmpty())
+                    return false;
+
+                char top = st.pop();
+
+                if((c==')' && top!='(') ||
+                   (c=='}' && top!='{') ||
+                   (c==']' && top!='['))
+                    return false;
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
