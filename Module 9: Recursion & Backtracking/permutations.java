c12632c4147030720a9618900b94730acd1c51d5
Aishwarya Hegde <aishmhegde@gmail.com>
2026-03-17 07:34:02 +0530

    Add files via upload

---
+++ Module 9: Recursion & Backtracking/permutations.java
@@ -0,0 +1,32 @@
+// LeetCode 46 – Permutations
+
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> permute(int[] nums) {
+
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(nums, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    void backtrack(int[] nums, List<Integer> temp, List<List<Integer>> result){
+
+        if(temp.size() == nums.length){
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        for(int num : nums){
+
+            if(temp.contains(num))
+                continue;
+
+            temp.add(num);
+            backtrack(nums, temp, result);
+            temp.remove(temp.size()-1);
+        }
+    }
+}
